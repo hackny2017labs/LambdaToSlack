5fd4128c6ff29232ae1e795c9793ac64cac3d2ea
Melanie Sawyer <melaniensawyer@gmail.com>
2017-07-13 00:52:43 -0400

    print event click type

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ url = 'https://hooks.slack.com/services/T59Q8770T/B67J2QKC1/8j0y6PSP9hqExb6o5OBT
 text = "<!channel> Someone is at the :door:! (624)"
 
 def handler(event, context):
-    print(event)
-    requests.post(url, json={"text": text + event})
-
+    requests.post(url, json={"text": text + event.clickType})
 
