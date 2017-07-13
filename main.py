c626eee28ef165a9d81e5e1ae688b35fe502e0f8
Melanie Sawyer <melaniensawyer@gmail.com>
2017-07-12 23:45:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import requests
 
 def handler(event, context):
-    requests.post('insert your url between these quotes',json={"text" : "Insert your message between these quotes, slack emotes such as :coffee: also work" })
+    requests.post('https://hooks.slack.com/services/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',json={"text" : "Someone is at the :door:! (624)" })
