63d6ed3c35a7ab2b8c37915b8c1d1b7b267821c4
Sakib Jalal <sakib.jalal@hioscar.com>
2017-07-12 23:50:08 -0400

    triggered

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import requests
 
+url = 'https://hooks.slack.com/services/services/T59Q8770T/B62RB8DEJ/mgtQWUwwoBGUQ0WqFe1KUV0O'
+
+text = "Someone is at the :door:! (624)"
+
 def handler(event, context):
-    requests.post('https://hooks.slack.com/services/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',json={"text" : "Someone is at the :door:! (624)" })
+    requests.post(url, json={"text": text})
