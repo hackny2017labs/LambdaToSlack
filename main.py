1b7b935170eb885d247fd198e8e81aad1ec88c42
Melanie Sawyer <melaniensawyer@gmail.com>
2017-07-13 00:15:49 -0400

    @channel

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 
 url = 'https://hooks.slack.com/services/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'
-
-text = "Someone is at the :door:! (624)"
+#Could change to <!here>
+text = "<!channel> Someone is at the :door:! (624)"
 
 def handler(event, context):
     requests.post(url, json={"text": text})
