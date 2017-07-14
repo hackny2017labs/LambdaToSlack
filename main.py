827cdf5b5c1163d6737d505ec39b382ddec8362b
Sruti Modekurty <sruti@twobulls.com>
2017-07-13 23:33:41 -0400

    Add double and single click

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
 
 url = 'https://hooks.slack.com/services/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'
-#Could change to <!here>
-text = "<!channel> Someone is at the :door:! (624)"
 
 def handler(event, context):
-    requests.post(url, json={"text": text + event.clickType})
-
+    suite = ""
+    if (event["clickType"] == "SINGLE"):
+        suite = "624"
+    elif (event["clickType"] == "DOUBLE"):
+        suite = "622"
+    text = "<!channel> Someone is at the " + suite + " :door:!"
+    requests.post(url, json={"text": text})
